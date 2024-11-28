5f5f3bdfc3a9a6cb53276c5e6724908b7fef5d89
karan <karan8872@gmail.com>
2024-11-29 00:35:45 +0530

    update InfoBox

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -1,4 +1,4 @@
-import style from "./css_modules/infoBox.module.css";
+import style from "./css_modules/InfoBox.module.css";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
